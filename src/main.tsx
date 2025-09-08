f066c0414681144bbb0d8a60c75da9ea677a28d9
Kay Do <khanh.do1610@gmail.com>
2025-09-07 20:34:07 -0400

    config: add React Router + Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+let router = createBrowserRouter([
+    {
+        Component: App,
+        children: [
+            {
+                path: "/",
+                element: <div>Home</div>,
+            },
+        ],
+    },
+]);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <RouterProvider router={router} />,
+    </StrictMode>
+);
